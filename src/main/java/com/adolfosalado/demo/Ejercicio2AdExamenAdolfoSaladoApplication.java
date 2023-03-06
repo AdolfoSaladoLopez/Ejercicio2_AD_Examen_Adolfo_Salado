f23113bf200aa5dbf9b7ab5ccd78b0ea1af7add8
AdolfoSaladoLopez <adolfo.s255446@cesurformacion.com>
2023-03-06 12:09:24 +0100

    Creación del proyecto y añadida configuración de BD.

---
+++ src/main/java/com/adolfosalado/demo/Ejercicio2AdExamenAdolfoSaladoApplication.java
@@ -0,0 +1,13 @@
+package com.adolfosalado.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio2AdExamenAdolfoSaladoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ejercicio2AdExamenAdolfoSaladoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adolfosalado/demo/Ejercicio2AdExamenAdolfoSaladoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adolfosalado.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio2AdExamenAdolfoSaladoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
